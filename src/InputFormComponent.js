341788319bc6198eb0dd0b4546e7027987a2c9b1
Marco Pulido <marcopulido00@gmail.com>
2023-06-07 13:58:08 +0800

    created input form component

--- src/InputFormComponent.js
+++ src/InputFormComponent.js
@@ -1,8 +1,9 @@
 function InputFormComponent() {
     return (  
-        <div className="bg-white">
-            <div>Text</div>
-        </div>
+        <div className="bg-white p-3">
+            <label className="block text-sm font-bold text-blue-900" for="nameId">Placeholder</label>
+            <input className="border rounded-s p-1 px-3" id="nameId" type="text" placeholder="Placeholder"></input>
+        </div> 
     );
 }
 
